2a9426f2d393d329438f3166772c035153dc47ca
SazharProject <dada>
2025-02-22 10:35:47 +0500

    changed anotherservice

--- src/main/java/org/example/AnotherService.java
+++ src/main/java/org/example/AnotherService.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AnotherService {
     public void doSomething() {
         System.out.println("Doing something in AnotherService!");
